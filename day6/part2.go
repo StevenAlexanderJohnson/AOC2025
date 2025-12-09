e4a45f28c9ab1e9bddadda12d6d26223c5f1e2e7
Steven Johnson <steveajohn1@gmail.com>
2025-12-09 15:39:44 +0000

    removing println from day 6

--- day6/part2.go
+++ day6/part2.go
@@ -5,7 +5,6 @@ import "fmt"
 func part2(homework Homework) (uint64, error) {
 	var output uint64
 	for _, question := range homework.Questions {
-		fmt.Println(question.Operation)
 		var result uint64
 		for _, cephalopodNumber := range question.CephalopodNumber() {
 			switch question.Operation {
